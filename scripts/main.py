97319145f6089a427bcbb0c62ce1e96d32fa209d
Innixma <innixma@gmail.com>
2016-11-17 20:32:06 -0600

    Added loss detection, memory
    Added game loss detection and added memory for the AI to use when
    backpropagating.

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,9 @@
-import win32gui, win32ui, win32con, win32api
+import win32gui, win32ui, win32con, win32api, win32com.client
 import random
 import numpy as np
 import scipy.misc as smp
 import time
+import process_move
 
 #Giant dictonary to hold key name and VK value
 VK_CODE = {'backspace':0x08,
@@ -159,7 +160,8 @@ def press(*args):
     accepts as many arguments as you want. e.g. press('left_arrow', 'a','b').
     '''
     for i in args:
-        win32api.keybd_event(VK_CODE[i], 0, 0, 0)
+        if i != 'none':
+            win32api.keybd_event(VK_CODE[i], 0, 0, 0)
 
 
 def release(*args):
@@ -169,15 +171,18 @@ def release(*args):
     e.g. release('left_arrow', 'a','b').
     '''
     for i in args:
-        win32api.keybd_event(VK_CODE[i], 0, win32con.KEYEVENTF_KEYUP, 0)
+        if i != 'none':
+            win32api.keybd_event(VK_CODE[i], 0, win32con.KEYEVENTF_KEYUP, 0)
 
 
 def rgb2gray(rgb):
     return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
 
 hwnd = win32gui.FindWindow(None, 'Open Hexagon 1.92 - by vittorio romeo')
+shell = win32com.client.Dispatch("WScript.Shell")
+shell.SendKeys('%')
 win32gui.SetForegroundWindow(hwnd)
-win32gui.MoveWindow(hwnd, 0, 0, 1280, 720, True)
+win32gui.MoveWindow(hwnd, 0, 0, 500, 500, True)
 
 if hwnd == 0:
     print('ERROR: Can\'t find window')
@@ -197,37 +202,58 @@ dataBitMap = win32ui.CreateBitmap()
 dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
 cDC.SelectObject(dataBitMap)
 
-num_iters = 100
+num_iters = 10000
+num_tries = 10
+memory_size = 100 # Number of frames to keep in memory for backpropagation
 
-keys = ['left_arrow', 'right_arrow']
+keys = np.array(['none', 'left_arrow', 'right_arrow', 'enter', 'esc'])
 
-start_time = time.time()
+# Run a number of tries
+for j in range(num_tries):
+    imageData = np.zeros((memory_size,h,w,3), dtype=np.uint8)
+    key_moves = np.zeros(memory_size, dtype=np.uint8)
+    process_move.reset_globs()
+    
+    press('enter')
+    time.sleep(.01)
+    release('enter')
+    
+    start_time = time.time()
+    prevKey = 'enter'
+    for i in range(num_iters):
+        cDC.BitBlt((0, 0), (w, h), dcObj, (0, 0), win32con.SRCCOPY)
+        
+        # Get image, memory of images
+        imageData[i % memory_size] = np.frombuffer(dataBitMap.GetBitmapBits(True), dtype=np.uint8).reshape((h, w, 4))[:,:,:-1][:,:,::-1]       
 
-press('enter')
-time.sleep(.01)
-release('enter')
-
-for i in range(num_iters):
-    cDC.BitBlt((0, 0), (w, h), dcObj, (0, 0), win32con.SRCCOPY)
-    key = random.choice(keys)
-    press(key)
-    # time.sleep(.01)
-    imageData = np.frombuffer(dataBitMap.GetBitmapBits(True), dtype=np.uint8).reshape((h, w, 4))
-    release(key)
+        # Function to process key given pixels
+        key = process_move.get_move(imageData[i % memory_size], keys)
+        
+        # Memory of moves
+        key_moves[i % memory_size] = np.where(keys==key)[0]
+        if key == None:
+            release(prevKey)
+        elif key == 'esc':
+            defeat = time.time()
+            print('Run ' + str(j) + ' survived ' + str("%.2f" % (defeat - start_time)) + 's')
+            break
+        else:
+            release(prevKey)
+            press(key)
+            prevKey = key
+    
+    press('esc')
+    time.sleep(.01)
+    release('esc')
+    print("--- %s fps ---" % ((i+1)/(time.time() - start_time))) 
 
 # Free Resources
 dcObj.DeleteDC()
 cDC.DeleteDC()
 win32gui.ReleaseDC(hwnd, wDC)
 win32gui.DeleteObject(dataBitMap.GetHandle())
-
-press('esc')
-time.sleep(.01)
-release('esc')
-
-print("--- %s fps ---" % (num_iters/(time.time() - start_time)))
-
-img = smp.toimage(imageData)       # Create a PIL image
-img.show()                      # View in default viewer
+    
+#img = smp.toimage(imageData)       # Create a PIL image
+#img.show()                      # View in default viewer
 
 
---
+++ scripts/process_move.py
@@ -0,0 +1,53 @@
+# By Nick Erickson, Benjamin Hillmann, Sivaraman Rajaganapathy
+# Contains main functions for selecting the optimal action
+
+import numpy as np
+
+meanLight = 0
+meanDiff = 0
+maxDiff = 0
+prevMean = 0
+c = 0
+
+# Resets globals
+def reset_globs():
+    global prevMean, meanDiff, meanLight, c, maxDiff
+    prevMean = 0
+    meanDiff = 0 # Currently unused
+    maxDiff = 0 # Current unused
+    prevMean = 0
+    c = 0
+    
+# Gets the optimal move
+def get_move(data, moves):
+    global prevMean, meanDiff, meanLight, c, maxDiff
+    c += 1
+    
+    ##############################################################
+    # Check if you lost, screen goes white when you lose, so check
+    curMean = np.mean(data)
+    if c <= 20:
+        maxDiff = 0
+        meanLight += curMean
+        if c == 20:
+            meanLight = meanLight / 20
+    else:
+        meanLight = meanLight * (9/10) + curMean * (1/10)
+        diff = curMean - prevMean
+        meanDiff = (meanDiff*((c-1)/c) + abs(diff)*(1/c))
+        maxDiff = max(maxDiff, abs(diff))
+        #print(meanDiff, diff)
+        threshold = (255 - meanLight) / 2
+        #print(threshold, diff, maxDiff)
+        if diff > threshold and c > 20:
+            return 'esc' # Lost!
+    prevMean = curMean
+    ##############################################################
+    
+    ##############################################################
+    # Compute the optimal move
+    optimal_move = moves[(c % 2) + 1] # Placeholder
+    ##############################################################
+    
+    return optimal_move
+    
