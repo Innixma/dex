e371db21243cdc923c764e19bbe34710c47d866b
Innixma <nickdean9@hotmail.com>
2016-12-08 01:22:55 -0600

    Improved loss detection, Added custom levels

--- scripts/process_move.py
+++ scripts/process_move.py
@@ -6,47 +6,60 @@ import numpy as np
 meanLight = 0
 meanDiff = 0
 maxDiff = 0
+maxMean = 0
 prevMean = 0
 c = 0
 
 # Resets globals
 def reset_globs():
-    global prevMean, meanDiff, meanLight, c, maxDiff
+    global prevMean, meanDiff, meanLight, c, maxDiff, maxMean
     prevMean = 0
     meanDiff = 0 # Currently unused
+    meanLight = 0
     maxDiff = 0 # Current unused
+    maxMean = 0
     prevMean = 0
     c = 0
     
 # Gets the optimal move
-def get_move(data, moves):
-    global prevMean, meanDiff, meanLight, c, maxDiff
+def get_move(data, moves, model):
+    global prevMean, meanDiff, meanLight, c, maxDiff, maxMean
     c += 1
     
     ##############################################################
     # Check if you lost, screen goes white when you lose, so check
     curMean = np.mean(data)
-    if c <= 20:
+    if c <= 60:
         maxDiff = 0
         meanLight += curMean
-        if c == 20:
-            meanLight = meanLight / 20
+        if c == 60:
+            meanLight = meanLight / 60
     else:
         meanLight = meanLight * (9/10) + curMean * (1/10)
         diff = curMean - prevMean
         meanDiff = (meanDiff*((c-1)/c) + abs(diff)*(1/c))
         maxDiff = max(maxDiff, abs(diff))
+        maxMean = max(curMean, maxMean)
         #print(meanDiff, diff)
-        threshold = (255 - meanLight) / 2
+        threshold = (230 - meanLight) / 2
         #print(threshold, diff, maxDiff)
-        if diff > threshold and c > 20:
+        #print(diff, threshold)
+        if diff > threshold and c > 60:
+            print(diff, threshold)
             return 'esc' # Lost!
     prevMean = curMean
     ##############################################################
     
     ##############################################################
     # Compute the optimal move
-    optimal_move = moves[(c % 2) + 1] # Placeholder
+    #optimal_move = moves[(c % 2) + 1] # Placeholder
+    if model:
+        results = model.predict(data.reshape(1, 1, 200, 200, 3)[:,:,:,:,0])
+        #print(results)
+        optimal_move = moves[np.argmax(results)]
+        print(optimal_move, results)
+    else:
+        optimal_move = moves[np.random.randint(0,3)]
     ##############################################################
     
     return optimal_move
